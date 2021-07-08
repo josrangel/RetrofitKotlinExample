bde52218d242a85e9903d9b7eb6119fc02423492
Josue <josrangel@hotmail.com>
2021-07-08 01:34:26 -0500

    - se agrega el interceptor OkHttpClient al objeto de retrofit para ver las peticiones en los Logs

--- app/src/main/java/com/example/retroplo/network/RandomRepo.kt
+++ app/src/main/java/com/example/retroplo/network/RandomRepo.kt
@@ -2,7 +2,8 @@ package com.example.retroplo.network
 
 import com.example.retroplo.entity.RandomClass
 import com.example.retroplo.util.Constants
-import org.jetbrains.anko.doAsync
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,8 +11,13 @@ import retrofit2.create
 
 class RandomRepo() {
     private val instanciaRetrofit by lazy {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val okclient: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(okclient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -22,6 +28,3 @@ class RandomRepo() {
         return response
     }
 }
-
-
-
