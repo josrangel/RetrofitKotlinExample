aa312fada19ee8ad91b748584e5786e5dae9db30
Josue <josrangel@hotmail.com>
2021-07-08 00:57:52 -0500

    - se agrega lo necesario para el RecyclerView - de la respuesta que ya se tenia, se convierte y se pasan los datos al adapter

--- app/src/main/java/com/example/retroplo/MainActivity.kt
+++ app/src/main/java/com/example/retroplo/MainActivity.kt
@@ -4,27 +4,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retroplo.adapter.PersonAdapter
+import com.example.retroplo.entity.ResultsItem
 import com.example.retroplo.network.RandomRepo
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textView: TextView;
+    lateinit var rvPersons: RecyclerView
+    var dataRV:List<ResultsItem> = ArrayList<ResultsItem>()
+    lateinit var adapter: PersonAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.textView)
+        setupRecyclerView()
+        getPersons()
+    }
+
+    fun setupRecyclerView() {
+        // getting the recyclerview by its id
+        rvPersons = findViewById(R.id.rvPersons)
+
+        // this creates a vertical layout Manager
+        rvPersons.layoutManager = LinearLayoutManager(this)
     }
 
 
-    fun getRandomPersonaaa(view:View){
+    fun getPersons() {
         var interceptor = RandomRepo()
         doAsync {
             var response = interceptor.getPersonRandomRepo()
             uiThread {
-                textView.text = response.body()?.results?.get(0)?.email
+                dataRV = response.body()?.results as List<ResultsItem>
+
+                // This will pass the ArrayList to our Adapter
+                adapter = PersonAdapter(dataRV)
+
+                // Setting the Adapter with the recyclerview
+                rvPersons.adapter = adapter
             }
         }
     }
