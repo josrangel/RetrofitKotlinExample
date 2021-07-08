f6bf25f934d205531377ecdf3369e10a3add9938
Josue <josrangel@hotmail.com>
2021-07-08 00:51:28 -0500

    - se modifica el endpoint para agregar el parametro results, para traer mas de un parametro

--- app/src/main/java/com/example/retroplo/network/EndPoint.kt
+++ app/src/main/java/com/example/retroplo/network/EndPoint.kt
@@ -3,15 +3,15 @@ package com.example.retroplo.network
 import com.example.retroplo.entity.RandomClass
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface EndPoint {
 
     //Tipo de request
     //Value es endpoint
-    @GET("/api/")
-    //get@GET("/api/")
+    @GET("/api")
     //getPerson metodo personalizado
     //Call palabra reservada de la Retrofit
     //RandomClass es el nombre del DataClas, de la llamada lo vas a guardar y convertir en RandomClass
-    fun getPerson(): Call<RandomClass>
+    fun getPerson(@Query("results") numResults: Int): Call<RandomClass>
 }
